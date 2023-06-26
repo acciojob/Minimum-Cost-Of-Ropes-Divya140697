4cf468dc937666f37d9d2b2eccc0bf3f9715f141
Divya140697 <125757414+Divya140697@users.noreply.github.com>
2023-06-26 15:57:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+function calculateMinimumCost(ropes) {
+  let totalCost = 0;
+
+  while (ropes.length > 1) {
+    ropes.sort((a, b) => a - b);
+
+    const rope1 = ropes.shift();
+    const rope2 = ropes.shift();
+    const newRope = rope1 + rope2;
+
+    totalCost += newRope;
+    ropes.push(newRope);
+  }
+
+  return totalCost;
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const input = document.getElementById('inputRopes').value;
+  const ropes = input.split(',').map(Number);
+
+  const minimumCost = calculateMinimumCost(ropes);
+
+  const resultElement = document.getElementById('result');
+  resultElement.textContent = minimumCost;
+}
+
+const form = document.getElementById('ropeForm');
+form.addEventListener('submit', handleSubmit);
