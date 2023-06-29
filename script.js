8883491c04e2ac0434917ac96127dbbcd701351a
Divya140697 <125757414+Divya140697@users.noreply.github.com>
2023-06-29 18:12:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,101 @@
-function calculateMinimumCost(ropes) {
+function findMinimumCostRopes(lengths) {
+  // Convert the comma-separated lengths into an array of integers
+  const ropes = lengths.split(',').map(Number);
+  
+  // Create a min heap using the ropes array
+  const minHeap = new MinHeap(ropes);
+  
   let totalCost = 0;
-
-  while (ropes.length > 1) {
-    ropes.sort((a, b) => a - b);
-
-    const rope1 = ropes.shift();
-    const rope2 = ropes.shift();
-    const newRope = rope1 + rope2;
-
-    totalCost += newRope;
-    ropes.push(newRope);
+  
+  // Connect the ropes until there is only one rope left in the heap
+  while (minHeap.size() > 1) {
+    const min1 = minHeap.extractMin();  // Extract the minimum length rope
+    const min2 = minHeap.extractMin();  // Extract the next minimum length rope
+    
+    const cost = min1 + min2;  // Calculate the cost of connecting the ropes
+    totalCost += cost;
+    
+    minHeap.insert(cost);  // Insert the connected rope back into the heap
   }
-
+  
   return totalCost;
 }
 
-function handleSubmit(event) {
-  event.preventDefault();
-
-  const input = document.getElementById('inputRopes').value;
-  const ropes = input.split(',').map(Number);
-
-  const minimumCost = calculateMinimumCost(ropes);
-
-  const resultElement = document.getElementById('result');
-  resultElement.textContent = minimumCost;
+// Implementation of MinHeap class for the heap data structure
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+  
+  insert(value) {
+    this.heap.push(value);
+    this.bubbleUp(this.heap.length - 1);
+  }
+  
+  extractMin() {
+    const minValue = this.heap[0];
+    const lastValue = this.heap.pop();
+    
+    if (this.heap.length > 0) {
+      this.heap[0] = lastValue;
+      this.sinkDown(0);
+    }
+    
+    return minValue;
+  }
+  
+  bubbleUp(index) {
+    const value = this.heap[index];
+    
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2);
+      const parentValue = this.heap[parentIndex];
+      
+      if (value >= parentValue) {
+        break;
+      }
+      
+      this.heap[parentIndex] = value;
+      this.heap[index] = parentValue;
+      
+      index = parentIndex;
+    }
+  }
+  
+  sinkDown(index) {
+    const length = this.heap.length;
+    const value = this.heap[index];
+    
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rightChildIndex = 2 * index + 2;
+      let smallestChildIndex = index;
+      
+      if (leftChildIndex < length && this.heap[leftChildIndex] < this.heap[smallestChildIndex]) {
+        smallestChildIndex = leftChildIndex;
+      }
+      
+      if (rightChildIndex < length && this.heap[rightChildIndex] < this.heap[smallestChildIndex]) {
+        smallestChildIndex = rightChildIndex;
+      }
+      
+      if (smallestChildIndex === index) {
+        break;
+      }
+      
+      this.heap[index] = this.heap[smallestChildIndex];
+      this.heap[smallestChildIndex] = value;
+      
+      index = smallestChildIndex;
+    }
+  }
+  
+  size() {
+    return this.heap.length;
+  }
 }
 
-const form = document.getElementById('ropeForm');
-form.addEventListener('submit', handleSubmit);
+// Example usage:
+//const input = "4,2,7,6,9";
+//const minimumCost = findMinimumCostRopes(input);
+//document.getElementById('result').textContent = minimumCost;
